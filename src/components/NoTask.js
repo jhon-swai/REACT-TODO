3cdcce3ee49b0985acfb91140bf89708c16f800c
jhon-swai <jhonswai@gmail.com>
2021-09-06 16:41:29 +0300

    Task component

---
+++ src/components/NoTask.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NoTask = () => {
+    return (
+        <div>
+            <h5 className="task">No Task To Show</h5>
+        </div>
+    )
+}
+
+export default NoTask
---
+++ src/components/Task.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { FaTimes} from "react-icons/fa";
+import { BiCheckbox, BiCheckboxChecked} from "react-icons/bi";
+const Task = ({ task, onDelete, onToggleReminder , onToggleTaskCompleted}) => {
+    return (
+        <div className="task" >
+            
+           
+            <div className="ch-task-columns">
+                
+
+                <div className={`task ${task.reminder ? 'reminder': ""}`} onDoubleClick={ () => onToggleReminder(task.id) }  >
+                    <h3>
+                        {task.text} 
+                    </h3>
+                    <p>{task.day}</p>
+                </div>
+                <div className="ch-checkbox">
+                    { task.completed ? 
+                    <BiCheckboxChecked style={{color: 'green'}} size={40} onClick={() => onToggleTaskCompleted(task.id) }/>: 
+                    <BiCheckbox size={40} onClick={() => onToggleTaskCompleted(task.id) }/>}   
+                </div>
+                
+
+            </div>
+            <div className="ch-delete-task">
+                    <FaTimes  style={{ color: 'red', cursor: 'pointer'}}
+                    onClick={() => onDelete(task.id)}/>
+                </div>
+            
+        
+            
+            
+            
+        </div>
+    )
+}
+
+export default Task
