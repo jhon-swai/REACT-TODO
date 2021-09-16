b910bf56201643ae351a533d0b5376d774bb97e9
jhon-swai <jhonswai@gmail.com>
2021-09-17 00:36:39 +0300

    converted to router link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,9 @@
+import {Link} from "react-router-dom"
 const Footer = () => {
     return (
         <div className="footer">
             <p> Copyright &copy; 2021</p>
-            <a href="/about">About</a>
+            <Link to="/about">About</Link>
         </div>
     )
 }
