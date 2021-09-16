efa5132fddbe7d05cce6d0c74d20e310ecc306d3
jhon-swai <jhonswai@gmail.com>
2021-09-16 22:30:05 +0300

    removed spaces

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,12 +3,8 @@ import { FaTimes} from "react-icons/fa";
 import { BiCheckbox, BiCheckboxChecked} from "react-icons/bi";
 const Task = ({ task, onDelete, onToggleReminder , onToggleTaskCompleted}) => {
     return (
-        <div className="task" >
-            
-           
+        <div className="task" >  
             <div className="ch-task-columns">
-                
-
                 <div className={`task ${task.reminder ? 'reminder': ""}`} onDoubleClick={ () => onToggleReminder(task.id) }  >
                     <h3>
                         {task.text} 
@@ -20,18 +16,12 @@ const Task = ({ task, onDelete, onToggleReminder , onToggleTaskCompleted}) => {
                     <BiCheckboxChecked style={{color: 'green'}} size={40} onClick={() => onToggleTaskCompleted(task.id) }/>: 
                     <BiCheckbox size={40} onClick={() => onToggleTaskCompleted(task.id) }/>}   
                 </div>
-                
-
             </div>
             <div className="ch-delete-task">
                     <FaTimes  style={{ color: 'red', cursor: 'pointer'}}
                     onClick={() => onDelete(task.id)}/>
-                </div>
-            
-        
-            
-            
-            
+            </div>
+   
         </div>
     )
 }
