d64f345de0275e8da568987e731da6847c8340db
jhon-swai <jhonswai@gmail.com>
2021-09-17 00:35:21 +0300

    main page with the tasks and about

---
+++ src/components/Body.js
@@ -0,0 +1,31 @@
+import AddTask from "./AddTask"
+import NoTask from "./NoTask"
+import Tasks from "./Tasks"
+import About from "./About"
+import { Route, Switch} from "react-router-dom"
+const Body = ({onAddTask, showAddTask, tasks, onDelete, onToggleReminder, onToggleTaskCompleted}) => {
+    return (
+        <div>
+            <Switch>
+                
+                <Route exact path="/">
+                {  
+                    tasks.length === 0 ? <NoTask />
+                    :<Tasks tasks={tasks} onDelete={onDelete} onToggleReminder={onToggleReminder} onToggleTaskCompleted={onToggleTaskCompleted}/>
+                }
+                </Route>
+                    
+               
+                <Route path="/addTask">
+                    <AddTask onAddTask={onAddTask}/>
+                </Route> 
+
+                <Route path="/about">
+                    <About />
+                </Route>              
+            </Switch>   
+        </div>
+    )
+}
+
+export default Body
