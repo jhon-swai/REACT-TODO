186f94562cbc88e082f085173aa09e66248241e9
jhon-swai <jhonswai@gmail.com>
2021-09-16 22:30:38 +0300

    removed the useLocation for router

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,18 +2,18 @@ import PropTypes from 'prop-types'
 
 import React from 'react'
 import Button from './Button'
-import { useLocation } from 'react-router-dom'
+
 
 const Header = ({ title, onShowAddTask, showAdd }) => {
-    const location = useLocation()
+    const location = false
     return (
         <header className="header">
             <h1>{title}</h1>
-            { location.pathname === '/' && (
-                showAdd ? 
-                <Button color="Red" text="Close" onClick={onShowAddTask}/> 
-                : <Button color="Green" text="Add" onClick={onShowAddTask}/>
-            )}           
+             {
+                 showAdd ? 
+                 <Button color="Red" text="Close" onClick={onShowAddTask}/> 
+                 : <Button color="Green" text="Add" onClick={onShowAddTask}/>
+             }       
         </header>
     )
 }
