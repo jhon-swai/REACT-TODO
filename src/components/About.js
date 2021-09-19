7079cd26b17bd84d593988c2958ebe20762f3011
jhon-swai <jhonswai@gmail.com>
2021-09-19 15:34:01 +0300

    Added some content and removed the link

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,17 @@
-import {Link} from 'react-router-dom'
+
 const About = () => {
     return (
         <div>
+            <h4>What is it</h4>
+            <hr/>
+            <p>This is a simple Task Tracker application that could help you write down all the tasks you want to do and check them when you are done.</p>
+            <br/>
+            <h4>Feedback</h4>
+            <hr/>
+            <p>Feel free to play around with it and give me some feedback</p>
+            <br/>
             <h4>Version 1.1.0</h4>
-            <Link to="/">Go back</Link>
+            
         </div>
     )
 }
